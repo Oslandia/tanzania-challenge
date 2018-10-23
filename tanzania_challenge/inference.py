e787cfcca677e6540793d50310ff59fd59681113
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-10-23 12:06:59 +0200

    inference: add modules for inference and postprocessing

---
+++ tanzania_challenge/inference.py
@@ -0,0 +1,111 @@
+"""
+"""
+
+import argparse
+import json
+import numpy as np
+import os
+from PIL import Image
+
+from mrcnn import model as modellib
+
+from tanzania_challenge import config
+from tanzania_challenge.train import prepare_dataset
+
+
+def predict_on_filename(datapath, img_size, filename):
+    """Do an instance segmentation prediction on a given image starting from
+    its location on file system, and provides resulting masks, class IDs and
+    prediction scores associated to each detected instance
+
+    Parameters
+    ----------
+    datapath : str
+        Path of data on file system
+    img_size : int
+        Size of tiled images to provide to the model
+    filename : str
+        Name of the image file into the testing dataset
+
+    Returns
+    -------
+    dict
+        Mask, class IDs and prediction scores for each detected instance
+    """
+    building_config = config.BuildingInferenceConfig()
+    model_path = os.path.join(datapath, "output",
+                              "instance_segmentation", "checkpoints")
+    model = modellib.MaskRCNN(mode="inference", config=building_config,
+                              model_dir=model_path)
+    weights_path = model.find_last()
+    model.load_weights(weights_path, by_name=True)
+    image = Image.open(os.path.join(datapath, "preprocessed", str(img_size),
+                                    "testing", "images", filename + ".tif"))
+    image_data = np.array(image)
+    prediction = model.detect(np.expand_dims(image_data, 0))[0]
+    results = {"masks": np.moveaxis(prediction["masks"], 2, 0).tolist(),
+               "class_ids": prediction["class_ids"].tolist(),
+               "scores": prediction["scores"].tolist()}
+    return results
+
+
+def predict_on_folder(datapath, img_size):
+    """Do an instance segmentation prediction on a given image starting from
+    its location on file system, and provides resulting masks, class IDs and
+    prediction scores associated to each detected instance
+
+    Parameters
+    ----------
+    datapath : str
+        Path of data on file system
+    img_size : int
+        Size of tiled images to provide to the model
+
+    Returns
+    -------
+    dict
+        Mask, class IDs and prediction scores for each detected instance
+    """
+    building_config = config.BuildingInferenceConfig()
+    model_path = os.path.join(datapath, "output",
+                              "instance_segmentation", "checkpoints")
+    model = modellib.MaskRCNN(mode="inference", config=building_config,
+                              model_dir=model_path)
+    weights_path = model.find_last()
+    model.load_weights(weights_path, by_name=True)
+
+    test_bd = prepare_dataset(os.path.join(datapath, "preprocessed"),
+                              img_size, "testing")
+    print(test_bd.num_images)
+    nb_images = test_bd.num_images
+    log = {}
+    for image_id in test_bd.image_ids[:nb_images]:
+        image = test_bd.load_image(image_id)
+        prediction = model.detect(np.expand_dims(image, 0))[0]
+        results = {"masks": np.moveaxis(prediction["masks"], 2, 0).tolist(),
+                   "class_ids": prediction["class_ids"].tolist(),
+                   "scores": prediction["scores"].tolist()}
+        with open(test_bd.image_info[image_id]["prediction_path"], "w") as fobj:
+            json.dump(results, fobj)
+        log[test_bd.image_info[image_id]["name"].split(".")[0]] = len(prediction["class_ids"])
+    return log
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description=("Infer building instances on"
+                                                  "images with a trained Mask-"
+                                                  "RCNN model."))
+    parser.add_argument('-d', '--datapath',
+                        default="./data/open_ai_tanzania",
+                        help=("Path towards dataset"))
+    parser.add_argument('-s', '--image-size',
+                        type=int,
+                        default=384,
+                        help=("Image size (in pixels)"))
+    parser.add_argument('-f', '--filename',
+                        help=("Image file name"))
+
+    args = parser.parse_args()
+
+    predict(args.datapath, args.image_size, args.filename)
---
+++ tanzania_challenge/postprocessing.py
@@ -0,0 +1,153 @@
+"""
+"""
+
+import cv2
+import numpy as np
+import os
+from osgeo import gdal, osr
+
+def flatten(data):
+    if len(data.shape) == 3:
+        return np.reshape(data, [-1, data.shape[2]])
+    else:
+        return np.reshape(data, [-1])
+
+
+def count_pixels(data):
+    if len(data.shape) == 3:
+        df = pd.DataFrame(flatten(data), columns=["red", "green", "blue"])                                   
+        return df.groupby(["red", "green", "blue"])["red"].count()
+    else:
+        return pd.Series(flatten(data)).value_counts()
+
+def count_pixels(data):
+    if len(data.shape) == 3:
+        df = pd.DataFrame(flatten(data), columns=["red", "green", "blue"])                                   
+        return df.groupby(["red", "green", "blue"])["red"].count()
+    else:
+        return pd.Series(flatten(data)).value_counts()
+
+
+def masking(data, pixel):
+    mask = np.zeros(data.shape[0] * data.shape[1], dtype=np.uint8)
+    if len(data.shape) == 3:
+        mask[np.all(flatten(data) == pixel, axis=1)] = 1
+    else:
+        mask[flatten(data) == pixel] = 1
+    return np.reshape(mask, [data.shape[0], data.shape[1]])
+
+
+def geo_project_label(data, datasource):
+    driver = gdal.GetDriverByName("MEM")
+    out_source = driver.Create("", data.shape[0], data.shape[1], 1, gdal.GDT_Int16)
+    out_source.SetProjection(datasource.GetProjection())
+    geotransform = list(datasource.GetGeoTransform())
+    geotransform[0] = geotransform[0] + (datasource.RasterXSize * geotransform[1]) * 10 / 20
+    geotransform[3] = geotransform[3] + (datasource.RasterYSize * geotransform[5]) * 10 / 20
+    out_source.SetGeoTransform(tuple(geotransform))
+    return out_source
+
+
+def get_image_features(ds):
+    width = ds.RasterXSize
+    height = ds.RasterYSize
+    gt = ds.GetGeoTransform()
+    minx = gt[0]
+    miny = gt[3] + height * gt[5] * 250/240
+    maxx = gt[0] + width * gt[1] * 250/240
+    maxy = gt[3]
+    srid = int(ds.GetProjection().split('"')[-2])
+    return {"west": minx, "south": miny, "east": maxx, "north": maxy,
+            "srid": srid, "width": width, "height": height}
+
+
+def pixel_to_coordinates(x, y, imfeatures):
+    lat = int(imfeatures["west"] + (imfeatures["east"]-imfeatures["west"]) * x / imfeatures["width"])
+    lon = int(imfeatures["south"] + (imfeatures["north"]-imfeatures["south"]) * y / imfeatures["height"])
+    return lat, lon
+
+
+def set_coordinates_as_x_y(lat, lon, srid):
+    """Transform coordinates into a (x,y)-compatible projection
+
+    Parameters
+    ----------
+    coordinates : dict of 4 float values
+        Min-x, min-y, max-x and max-y coordinates with keys 'west', 'south',
+    'east', 'north'
+    image_filename : str
+        Image path on the file system (will be used to get the original image
+    projection)
+    srid : int
+        Geographical projection
+
+    Returns
+    -------
+    dict
+        Bounding box of the image (west, south, east, north coordinates)
+    """
+    source = osr.SpatialReference()
+    source.ImportFromEPSG(srid)
+    target = osr.SpatialReference()
+    target.ImportFromEPSG(4326)
+    transform = osr.CoordinateTransformation(source, target)
+    x, y = transform.TransformPoint(lat, lon)[:2]
+    return y, x
+
+
+def pixel_to_latlon(x, y, imfeatures):
+    coordinates = pixel_to_coordinates(x, y, imfeatures)
+    return set_coordinates_as_x_y(coordinates[0],
+                                  coordinates[1],
+                                  imfeatures["srid"])
+
+
+def build_geom(building, imfeatures=None, pixel=False, min_x=2500, min_y=2500):
+    feature = []
+    for point in building:
+        if pixel:
+            feature.append((int(min_x + point[0][0]), min_y + int(point[0][1])))
+        else:
+            feature.append(pixel_to_latlon(point[0][0], point[0][1], imfeatures))
+    feature.append(feature[0])
+    return feature
+
+
+def extract_geometry(mask, structure):
+    """
+    """
+    denoised = cv2.morphologyEx(mask, cv2.MORPH_OPEN, structure)
+    grown = cv2.morphologyEx(denoised, cv2.MORPH_CLOSE, structure)
+    _, contours, hierarchy = cv2.findContours(grown, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    polygons = [cv2.approxPolyDP(c, epsilon=0.01*cv2.arcLength(c, closed=True), closed=True)
+                for c in contours]
+    return polygons
+
+
+def add_polygon(polygon, class_id, score, results, geofeatures, min_x=0, min_y=0):
+    """
+    """
+    feature = build_geom(polygon, imfeatures=geofeatures, pixel=False, min_x=min_x, min_y=min_y)
+    geom = geojson.Polygon([feature])
+    shape = shgeom.shape(geom)
+    pixel_feature = build_geom(polygon, pixel=True, min_x=0, min_y=0)
+    pixel_geom = geojson.Polygon([pixel_feature])
+    pixel_shape = shgeom.shape(pixel_geom)
+    predictions = np.zeros([3])
+    predictions[class_id-1] = score
+    return results.append([*predictions, shape.wkt, pixel_shape.wkt])
+
+
+def postprocess(predictions, features, min_x, min_y):
+    """
+    """
+    results = [] 
+    structure = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    mask = predictions["masks"]
+    class_ids = predictions["class_ids"]
+    scores = predictions["scores"]
+    if len(mask) > 0:
+        polygon = extract_geometry(mask, structure)
+        add_polygon(polygon[0], class_ids, scores, results,
+                    features, min_x, min_y)
+    return results
